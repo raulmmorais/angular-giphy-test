ea1865de007e1930dbab45b8e7c01548a13cd651
raulmmorais <raulmmorais@gmail.com>
2019-04-18 13:14:02 -0300

    1 file modified

---
+++ src/app/giphy/giphy-search/giphy-search.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+
+import { giphySearchComponent } from './giphy/giphy-search/giphy-search.component';
+
+@NgModule({
+  imports: [],
+  exports: [ giphySearchComponent ],
+  declarations: [ giphySearchComponent ],
+  providers: []
+})
+
+export class giphySearchComponentModule{};
